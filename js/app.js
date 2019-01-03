d00b6ba6ef2c92cc5353d862149dcfc1eff997e0
James Benson <you@example.com>
2019-01-03 16:40:41 +0000

    Basic HTML layout and CSS

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ console.log("Hello")
 
 document.addEventListener('DOMContentLoaded', ()=>{
   const h1 = document.querySelector('h1')
-  h1.textContent = 'App.js is loaded'
+  // h1.textContent = 'App.js is loaded'
 })
